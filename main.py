579e35992523bb57b1a0de2f0a8d0e4b063ba2fd
Yerkimbekov Alisher <simpgoogls@gmail.com>
2019-08-23 04:02:49 +0600

    changes in code

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from telebot import types
 bot = telebot.TeleBot("970640600:AAGbcjHy1cO97SdUDChVkwNNyYU0ueHSttw")
 
 while (1):
-	bot.send_message(chat_id='@saveeyes', text='Train'),
-	time.sleep(60)
+	bot.send_message(chat_id='@saveeyes', text='Сделай упражнение глаз!'),
+	time.sleep(7200)
 
 bot.polling()
