5f5cf71cff88c697e44a4cce85df9adf81a1169a
Yerkimbekov Alisher <simpgoogls@gmail.com>
2019-08-23 23:53:50 +0600

    kek

--- main.py
+++ main.py
@@ -4,9 +4,50 @@ from telebot import types
 
 bot = telebot.TeleBot("970640600:AAGbcjHy1cO97SdUDChVkwNNyYU0ueHSttw")
 
-while (1):
-	msg = bot.send_message(chat_id='@saveeyes', text='Сделай упражнение для глаз!'),
-	time.sleep(7200)
+def add():
+	msg = bot.send_message(chat_id='@saveeyes', text='Пора делать упражнение для глаз!')
+
+def dele():
 	bot.delete_message(chat_id='@saveeyes', message_id=msg.message_id)
+
+msg = bot.send_message(chat_id='@saveeyes', text='train!')
+schedule.every().day.at("00:00:00").do(dele)
+schedule.every().day.at("00:00:00").do(add)
+
+schedule.every().day.at("02:00:00").do(dele)
+schedule.every().day.at("02:00:00").do(add)
+
+schedule.every().day.at("04:00:00").do(dele)
+schedule.every().day.at("04:00:00").do(add)
+
+schedule.every().day.at("06:00:00").do(dele)
+schedule.every().day.at("06:00:00").do(add)
+
+schedule.every().day.at("08:00:00").do(dele)
+schedule.every().day.at("08:00:00").do(add)
+
+schedule.every().day.at("10:00:00").do(dele)
+schedule.every().day.at("10:00:00").do(add)
+
+schedule.every().day.at("12:00:00").do(dele)
+schedule.every().day.at("12:00:00").do(add)
+
+schedule.every().day.at("14:00:00").do(dele)
+schedule.every().day.at("14:00:00").do(add)
+
+schedule.every().day.at("16:00:00").do(dele)
+schedule.every().day.at("16:00:00").do(add)
+
+schedule.every().day.at("18:00:00").do(dele)
+schedule.every().day.at("18:00:00").do(add)
+
+schedule.every().day.at("20:00:00").do(dele)
+schedule.every().day.at("20:00:00").do(add)
+
+schedule.every().day.at("22:00:00").do(dele)
+schedule.every().day.at("22:00:00").do(add)
+
+while (1):
+	schedule.run_pending();
 
 bot.polling()
