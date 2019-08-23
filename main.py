54afac626e1244a75b08dd42bb107373e90eb80d
Yerkimbekov Alisher <simpgoogls@gmail.com>
2019-08-23 18:04:48 +0600

    typo

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from telebot import types
 bot = telebot.TeleBot("970640600:AAGbcjHy1cO97SdUDChVkwNNyYU0ueHSttw")
 
 while (1):
-	msg = bot.send_message(chat_id='@saveeyes', text='Сделай упражнение глаз!'),
+	msg = bot.send_message(chat_id='@saveeyes', text='Сделай упражнение для глаз!'),
 	time.sleep(7200)
 	bot.delete_message(chat_id='@saveeyes', message_id=msg.message_id)
 
