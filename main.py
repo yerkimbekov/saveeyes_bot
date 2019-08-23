e55bd6a8b0a7fe7800e8037afd37f072baf96d42
Yerkimbekov Alisher <simpgoogls@gmail.com>
2019-08-24 00:00:52 +0600

    kek

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def add():
 def dele():
 	bot.delete_message(chat_id='@saveeyes', message_id=msg.message_id)
 
-msg = bot.send_message(chat_id='@saveeyes', text='train!')
+msg = bot.send_message(chat_id='@saveeyes', text='Пора делать упражнение для глаз!')
 schedule.every().day.at("00:00:00").do(dele)
 schedule.every().day.at("00:00:00").do(add)
 
