7e5d2d9b7de68774f9530f70c0a6eb7471d6c8b9
Yerkimbekov Alisher <simpgoogls@gmail.com>
2019-08-27 16:55:45 +0600

    updated and upgraded

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 import telebot
 from telebot import types
 
-
+bot = telebot.TeleBot("970640600:AAGbcjHy1cO97SdUDChVkwNNyYU0ueHSttw")
 
 def add():
 	global msg
@@ -11,45 +11,16 @@ def add():
 
 def dele():
 	bot.delete_message(chat_id='@saveeyes', message_id=msg.message_id)
+	time.sleep(1)
+
+time.sleep(3600)
 
 msg = bot.send_message(chat_id='@saveeyes', text='Пора делать упражнение для глаз!')
-schedule.every().day.at("00:00:00").do(dele)
-schedule.every().day.at("00:00:00").do(add)
-
-schedule.every().day.at("02:00:00").do(dele)
-schedule.every().day.at("02:00:00").do(add)
-
-schedule.every().day.at("04:00:00").do(dele)
-schedule.every().day.at("04:00:00").do(add)
-
-schedule.every().day.at("06:00:00").do(dele)
-schedule.every().day.at("06:00:00").do(add)
-
-schedule.every().day.at("08:00:00").do(dele)
-schedule.every().day.at("08:00:00").do(add)
-
-schedule.every().day.at("10:00:00").do(dele)
-schedule.every().day.at("10:00:00").do(add)
-
-schedule.every().day.at("12:00:00").do(dele)
-schedule.every().day.at("12:00:00").do(add)
-
-schedule.every().day.at("14:00:00").do(dele)
-schedule.every().day.at("14:00:00").do(add)
-
-schedule.every().day.at("16:00:00").do(dele)
-schedule.every().day.at("16:00:00").do(add)
-
-schedule.every().day.at("18:00:00").do(dele)
-schedule.every().day.at("18:00:00").do(add)
-
-schedule.every().day.at("20:00:00").do(dele)
-schedule.every().day.at("20:00:00").do(add)
-
-schedule.every().day.at("22:00:00").do(dele)
-schedule.every().day.at("22:00:00").do(add)
+schedule.every(2).hours.do(dele)
+schedule.every(2).hours.do(add)
 
 while (1):
 	schedule.run_pending();
+	time.sleep(1)
 
 bot.polling()
