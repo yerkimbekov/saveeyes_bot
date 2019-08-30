c038a5698894b07eefc3870504e65a3adc755308
yerkimbekov <37952414+yerkimbekov@users.noreply.github.com>
2019-08-31 02:24:54 +0600

    Add files via upload
    asd

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import datetime
 import schedule
 import time
 import telebot
@@ -14,14 +13,17 @@ def dele():
 	bot.delete_message(chat_id='@saveeyes', message_id=msg.message_id)
 	time.sleep(1)
 
-now = datetime.datetime.now()
-tr = 0
+time.sleep(5700)
+
+# msg = bot.send_message(chat_id='@saveeyes', text='Пора делать упражнение для глаз!')
+# schedule.every(2).hours.do(dele)
+# schedule.every(2).hours.do(add)
 
 while (1):
-	if (tr == 0) and ((now.hour == 0 and now.minute == 40) or  (now.hour == 2 and now.minute == 0) or  (now.hour == 4 and now.minute == 0) or  (now.hour == 6 and now.minute == 0) or  (now.hour == 8 and now.minute == 0) or  (now.hour == 10 and now.minute == 0) or  (now.hour == 12 and now.minute == 0) or  (now.hour == 14 and now.minute == 0) or  (now.hour == 16 and now.minute == 0) or  (now.hour == 18 and now.minute == 0) or  (now.hour == 20 and now.minute == 0) or  (now.hour == 22 and now.minute == 0)):
-		tr = 1
-		add()
-		time.sleep(60);
-		tr = 0;
+	add()
+	time.sleep(10800)
+	dele()
+	# schedule.run_pending();
+	# time.sleep(1)
 
 bot.polling()
