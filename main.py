b91b1bef6db88e4cc5cb25145abb0409e0316f68
Yerkimbekov Alisher <37952414+yerkimbekov@users.noreply.github.com>
2019-10-17 14:14:02 +0600

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ def dele():
 
 schedule.every().day.at("00:00:20").do(add)
 schedule.every().day.at("03:00:20").do(add)
+schedule.every().day.at("06:00:20").do(add)
 schedule.every().day.at("09:00:20").do(add)
+schedule.every().day.at("12:00:20").do(add)
 schedule.every().day.at("15:00:20").do(add)
-schedule.every().day.at("18:00:20").do(add)
-schedule.every().day.at("21:00:20").do(add)
 	
 while (1):
 	schedule.run_pending()
